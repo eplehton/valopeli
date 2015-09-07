86c37d22f074b9a28729acdfc66aad9ae5f4a4b5
Caudate <erno.paunonen@hotmail.com>
2015-09-07 17:17:39 +0300

    Kuvat korvattu piirretyillä ympyröillä

--- valopeli.js
+++ valopeli.js
@@ -1,9 +1,10 @@
 var reds = [];
 var nextPress = 0;
+var testResults = [];
+var stopGame = false;
 
-function disco() {
-
-	var flashRedsintervals = 2000;
+function game(isStaticGame, flashRedsintervals) {
+	
 	
 	for (var i=0; i<500; i++) {
 		
@@ -12,65 +13,108 @@ function disco() {
 			reds.push(x); 
 		} else {
 			while (reds[ reds.length-1 ] == x) {
-				x = Math.floor(Math.random() * (9-1) + 1)
+				x = Math.floor(Math.random() * (10-1) + 1)
 			}
 			reds.push(x);
 		}
 	}
 	
 	function flashReds(position) {
-		var cellId = "cellButton" + reds[position];
-		var randomNumber = Math.floor(Math.random() * (60));
-		if(position > 0) {
-			switchImageColor("cellButton" + reds[position - 1], "gray");
-		}
-		
-		switchImageColor(cellId, "red");
-		
-		if (position < reds.length - 1) {
-			setTimeout( function() { flashReds(position+1); }, flashRedsintervals );
-			if (randomNumber < 3) {
-				flashBlue(position);
+		if (stopGame == false) {
+			var cellId = "#cellCircle" + reds[position];
+			var randomNumber = Math.floor(Math.random() * (60));
+			if(position > 0) {
+				switchColorGray("#cellCircle" + reds[position - 1]);
 			}
-			if (randomNumber == 4) {
-				flashDistractor(position, "green");
+			
+			switchColorRed(cellId);
+			
+			if (position < reds.length - 1) {
+				setTimeout( function() { flashReds(position+1); }, flashRedsintervals);
+				if (randomNumber < 3) {
+					flashBlue(position);
+				}
+				if (randomNumber == 4) {
+					flashDistractor(position);
+				}
 			}
-		}
-		
-		if (flashRedsintervals < 200) {
-			flashRedsintervals--;
+			
+			if(isStaticGame == false) {
+				if (flashRedsintervals < 200) {
+					flashRedsintervals--;
+				} else {
+					flashRedsintervals -= 2;
+				}			
+			}
 		} else {
-			flashRedsintervals -= 2;
+			testResults.push(flashRedsintervals);
 		}
 	}
 	
 	function flashBlue(position) {
-		var nextCellId = "cellButton" + reds[position + 1];
-		switchImageColor(nextCellId, "blue");
+		var nextCellId = "#cellCircle" + reds[position + 1];
+		switchColorBlue(nextCellId);
 	}
 	
-	function flashDistractor(position, color) {
-		var rNumber = Math.floor(Math.random() * (9-1) + 1);
+	function flashDistractor(position) {
+		var rNumber = Math.floor(Math.random() * (10-1) + 1);
 		console.log(rNumber);
 		if(rNumber != position) {
-			switchImageColor("cellButton" + rNumber, "green");
-			setTimeout(function() {switchImageColor("cellButton" + rNumber, "gray");}, flashRedsintervals);
+			switchColorGreen("#cellCircle" + rNumber);
+			setTimeout(function() {switchColorGray("#cellCircle" + rNumber);}, flashRedsintervals - 1);
 		} else {
-			flashDistractor(position, "green");
+			flashDistractor(position);
 		}
 	}
 	
 	flashReds(0);
 }
 
-function switchCellColor(cellId, color) {
-    var e = document.getElementById(cellId);
-	e.style.backgroundColor = color;
+
+function switchColorRed(id) {
+	var circle = $(id);
+	circle.css("background", "-moz-radial-gradient(center, ellipse cover, #ff0000 0%, #ff0000 40%, #000000 100%)");
+	circle.css("background", "-webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #ff0000), color-stop(40%, #ff0000), color-stop(100%, #000000))");
+	circle.css("background", "-webkit-radial-gradient(center, ellipse cover, #ff0000 0%, #ff0000 40%, #000000 100%)");
+	circle.css("background", "-o-radial-gradient(center, ellipse cover, #ff0000 0%, #ff0000 40%, #000000 100%)");
+	circle.css("background", "-ms-radial-gradient(center, ellipse cover, #ff0000 0%, #ff0000 40%, #000000 100%)");
+	circle.css("background", "radial-gradient(ellipse at center, #ff0000 0%, #ff0000 40%, #000000 100%)");
+	circle.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='#ff0000', endColorstr='#000000',GradientType=1 )");
 }
 
-function switchImageColor(cellButtonId, color) {
-	document.getElementById(cellButtonId).src="images/button_" + color + ".png";
+function switchColorGray(id) {
+	var circle = $(id);
+	circle.css("background", "-moz-radial-gradient(center, ellipse cover, #ffffff 0%, #000000 100%)");
+	circle.css("background", "-webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #ffffff), color-stop(100%, #000000))");
+	circle.css("background", "-webkit-radial-gradient(center, ellipse cover, #ffffff 0%, #000000 100%)");
+	circle.css("background", "-o-radial-gradient(center, ellipse cover, #ffffff 0%, #000000 100%)");
+	circle.css("background", "-ms-radial-gradient(center, ellipse cover, #ffffff 0%, #000000 100%)");
+	circle.css("background", "radial-gradient(ellipse at center, #ffffff 0%, #000000 100%)");
+	circle.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#000000',GradientType=1 )");
 }
+
+function switchColorBlue(id) {
+	var circle = $(id);
+	circle.css("background", "-moz-radial-gradient(center, ellipse cover, #0000ff 0%, #0000ff 40%, #000000 100%)");
+	circle.css("background", "-webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #0000ff), color-stop(40%, #0000ff), color-stop(100%, #000000))");
+	circle.css("background", "-webkit-radial-gradient(center, ellipse cover, #0000ff 0%, #0000ff 40%, #000000 100%)");
+	circle.css("background", "-o-radial-gradient(center, ellipse cover, #0000ff 0%, #0000ff 40%, #000000 100%)");
+	circle.css("background", "-ms-radial-gradient(center, ellipse cover, #0000ff 0%, #0000ff 40%, #000000 100%)");
+	circle.css("background", "radial-gradient(ellipse at center, #0000ff 0%, #0000ff 40%, #000000 100%)");
+	circle.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='#0000ff', endColorstr='#000000',GradientType=1 )");
+}
+
+function switchColorGreen(id) {
+	var circle = $(id);
+	circle.css("background", "-moz-radial-gradient(center, ellipse cover, #00FF00 0%, #00FF00 40%, #000000 100%)");
+	circle.css("background", "-webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #00FF00), color-stop(40%, #00FF00), color-stop(100%, #000000))");
+	circle.css("background", "-webkit-radial-gradient(center, ellipse cover, #00FF00 0%, #00FF00 40%, #000000 100%)");
+	circle.css("background", "-o-radial-gradient(center, ellipse cover, #00FF00 0%, #00FF00 40%, #000000 100%)");
+	circle.css("background", "-ms-radial-gradient(center, ellipse cover, #00FF00 0%, #00FF00 40%, #000000 100%)");
+	circle.css("background", "radial-gradient(ellipse at center, #00FF00 0%, #00FF00 40%, #000000 100%)");
+	circle.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='#00FF00', endColorstr='#000000',GradientType=1 )");
+}
+
 $(document).ready( function() {
 	
 	$("#pietimerArea").pietimer({
@@ -79,7 +123,8 @@ $(document).ready( function() {
 		height: 500,
 		width: 500	
 	}, function(){
-		disco();
+		$("#pietimerArea").css("z-index", "-1");
+		game(false, 500);
 	});
 	
 	$("#gametable").find("td").click(
@@ -91,10 +136,10 @@ $(document).ready( function() {
 		}
 	);
 	
-	$("#gametable").find("img").click(
+	$("#gametable").find("div").click(
 		function(ev) {
-			if (ev.target.id != "cellButton" + reds[nextPress]) {
-				location.reload();
+			if (ev.target.id != "cellCircle" + reds[nextPress]) {
+				stopGame = true;
 			}
 			nextPress++;
 		}
@@ -103,7 +148,6 @@ $(document).ready( function() {
 	$("startButton").click(
 		function(ev) {
 			console.log("sfhso");
-			disco();
 		}
 	);
 	
